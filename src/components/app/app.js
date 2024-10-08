861e2eac24233862f97b5aebaf06a327b124e951
Evgeniy Ivanov <irelion_synergy@mail.ru>
2024-10-08 21:46:21 +0300

    REACT-0002: added active/completed and delete task

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import './app.css';
 
 import AppHeader from "../app-header";
 import AppMain from "../app-main";
 
 
-const App = () => {
-    const tasks = [
-        {id: 1, clazzName: 'completed', description: 'Completed task', timeAgo: new Date(2024, 9, 2)},
-        {id: 2, clazzName: 'editing', description: 'Editing task', timeAgo: new Date(2014, 6, 2)},
-        {id: 3, clazzName: 'view', description: 'Active task', timeAgo: new Date(2014, 6, 2)},
-    ];
+export default class App extends Component{
+    state = {
+        tasks: [
+            {id: 1, description: 'Completed task', timeAgo: new Date(2024, 9, 2)},
+            {id: 2,  description: 'Editing task', timeAgo: new Date(2014, 6, 2)},
+            {id: 3,  description: 'Active task', timeAgo: new Date(2014, 6, 2)},
+        ]
+    };
 
-    return (
-        <section className='todoapp'>
-            <AppHeader/>
-            <AppMain tasks={tasks}/>
-        </section>
-    )
+    deleteTask = (id) => {
+        this.setState(({tasks}) => {
+            const newTasks = tasks.filter((task) => task.id !== id);
+            return {
+                tasks: newTasks,
+            };
+        });
+    };
+
+    render() {
+        const {tasks} = this.state;
+
+        return (
+            <section className='todoapp'>
+                <AppHeader/>
+                <AppMain
+                    tasks={tasks}
+                    deleteTask={(id) =>{
+                        this.deleteTask(id);
+                    }}
+                />
+            </section>
+        );
+    }
 };
-
-export default App;
