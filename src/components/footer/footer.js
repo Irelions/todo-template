eb0dbd597f1f1b16091d34dc6be7330e0e8e9c8a
irelion <irelion@mail.ru>
2024-10-15 17:04:14 +0300

    REACT-0004: refactoring, added eslint and prettier

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,27 +1,21 @@
-import React from "react";
 import './footer.css';
 
-import TasksFilter from "../tasks-filter";
+import TasksFilter from '../tasks-filter';
 
-const Footer = ({setActiveFilter, clearAllCompleted, countActiveTask}) => {
-
-    return (
-        <footer className='footer'>
-            <span className='todo-count'> {countActiveTask} items left</span>
-            <TasksFilter
-                setActiveFilter={(filterId) => {
-                    setActiveFilter(filterId)
-                }}
-            />
-            <button
-                className='clear-completed'
-                onClick={clearAllCompleted}
-            >
-                Clear completed
-            </button>
-        </footer>
-
-    );
-};
+function Footer({ setActiveFilter, clearAllCompleted, countActiveTask }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count"> {countActiveTask} items left</span>
+      <TasksFilter
+        setActiveFilter={(filterId) => {
+          setActiveFilter(filterId);
+        }}
+      />
+      <button type="button" className="clear-completed" onClick={clearAllCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
+}
 
 export default Footer;
